922bf2d9b915fc895ae3b468f743f2768c091e33
YahyaOsama2612 <oyahya868@gmail.com>
2024-08-24 02:20:55 +0400

    finalEdition

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@
 const Home = () =>{
     return(
         <div id="home">
-            <img src="/src/assets/poster2.jpg" alt="" />
+            <img src="../../public/poster2.jpg" alt="" />
         </div>
     )
 }
